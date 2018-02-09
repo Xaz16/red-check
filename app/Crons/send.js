f1a5d15b42b3cb45f80c39155538948ad0316ba0
gxaz16@gmail.com <gxaz16@gmail.com>
2018-02-09 08:04:12 +0300

    change cron

--- app/Crons/send.js
+++ app/Crons/send.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 
-const start = (handler, arg) => cron.schedule('0 * * * *', () => {
+const start = (handler, arg) => cron.schedule('* 15-17 * * *', () => {
   handler.apply(null, arg);
 } , true);
 
